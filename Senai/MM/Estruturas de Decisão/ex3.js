349bea7082b7ffede16ad1c5ece167d4d405415e
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-09 10:31:10 -0300

    feat: exercicio 03

---
+++ Senai/MM/Estruturas de Decisão/ex3.js
@@ -0,0 +1,18 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+// Entra o número
+let numero = prompt("Digite um número: ");
+
+// divide por 2 e pega o resto
+numero = numero % 2;
+
+// se for 0 é impar
+if (numero == 0) {
+    alert("O número é par");
+} 
+else {
+    alert("O número é impar");
+}
+// mostra se impar ou par
