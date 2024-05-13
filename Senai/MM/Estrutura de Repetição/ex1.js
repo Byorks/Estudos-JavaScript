b256b8ea8a3325afc69eca9762d29917aca25326
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-13 10:51:02 -0300

    feat: repeticao exercicio 01

--- Senai/MM/Estrutura de Repetição/ex1.js
+++ Senai/MM/Estrutura de Repetição/ex1.js
@@ -1,1 +1,7 @@
-// Imprimir números de 1 a 10 utilizando o loop for
+// 1. Imprimir números de 1 a 10 utilizando o loop for
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+for (let contador = 0; contador < 10; contador++){
+    console.log(numeros[contador]);
+}
---
+++ Senai/MM/Estrutura de Repetição/ex2.js
@@ -0,0 +1,7 @@
+// 2. Imprimir números pares de 1 a 10.
+
+for (i = 0; i < 10; i++){
+    if ( i % 0 == 0) {
+        console.log()
+    }
+}
