c9b97826b688f5d45cc42467aea54d81020b15c0
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-13 12:03:57 -0300

    exercicio 02

--- Senai/MM/Estrutura de Repetição/ex2.js
+++ Senai/MM/Estrutura de Repetição/ex2.js
@@ -1,7 +1,7 @@
 // 2. Imprimir números pares de 1 a 10.
 
 for (i = 0; i < 10; i++){
-    if ( i % 0 == 0) {
-        console.log()
+    if ( i % 2 == 0) {
+        console.log(`${i} é um número par!`);
     }
 }
