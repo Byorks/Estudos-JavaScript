1c66246b291845de42f14e576da08a28b75f653b
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-13 13:45:31 -0300

    fix: exercicio 05

--- Senai/MM/Estrutura de Repetição/ex5.js
+++ Senai/MM/Estrutura de Repetição/ex5.js
@@ -1,6 +1,19 @@
 // 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+numX = 1;
+numY = 1;
 
-for (numX = 1; numX <= 10; numX++) {
-    numY = 1;
-    console.log (`${numX}, ${numY}`)
+
+
+for (cont = 1; cont <= 10; cont++) {
+    if (cont == 1) {
+    console.log (`${numX}`)
+    console.log (`${numY}`)
+    }
+    
+    let fibbo = numX + numY;
+
+    console.log (`${fibbo}`)
+
+    numY = numX;
+    numX = fibbo;
 }
