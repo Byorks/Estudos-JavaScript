81d543aaa2fe014487edde576796a21f29550906
Byorks <vanessabyorksp@gmail.com>
2024-05-14 14:48:50 -0300

    feat: exercicio 10

---
+++ Senai/MM/Estrutura de Repetição/ex10.js
@@ -0,0 +1,10 @@
+let entrada = 1;
+
+while (entrada !== 0) {
+    entrada = parseFloat(prompt("Digite um número, se digitar 0 vai parar o programa."))
+    
+    console.log(`O número digitado foi ${entrada}`)
+    
+}
+
+console.log(`Você digitou ${entrada}, fim do programa`)
