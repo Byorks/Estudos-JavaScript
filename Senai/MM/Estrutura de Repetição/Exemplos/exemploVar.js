52ccfe96bfc6d7fe0396acce7f6ed2d85cc23af8
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-14 10:21:09 -0300

    feat: exercicios 06 e 07

---
+++ Senai/MM/Estrutura de Repetição/Exemplos/exemploVar.js
@@ -0,0 +1,10 @@
+// declarei uma var, e coloquei o valor 24 na IdadeThiago
+let idadeThiago = 24;
+
+idadeThiago = 24 + 1;
+
+const AnoDeNascimentoThiago = 2000; 
+
+AnoDeNascimentoThiago = 2000 + 1; 
+
+console.log(AnoDeNascimentoThiago)
---
+++ Senai/MM/Estrutura de Repetição/ex7.js
@@ -0,0 +1,12 @@
+// 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let numA = 0;
+let numB = 1;
+
+
+while (numA <= 100) {
+    numA ++ 
+    numB += numA;
+}
+
+console.log(numB)
