55c789b484681bdc98d9b03994431b7f84ae0494
Byorks <vanessabyorksp@gmail.com>
2024-05-16 08:51:30 -0300

    feat: exemplo funcoes e objetos

--- Senai/MM/Funcoes/exObjeto.js
+++ Senai/MM/Funcoes/exObjeto.js
@@ -5,3 +5,13 @@ let professor = {
     sobrenome: "Nascimento",
     idade: 25
 }
+
+// descrever um objeto do mundo e trazer para  o código
+// variavel com varias caracteristicas 
+let produto = {
+    nome: "Controle Remoto",
+    marca: "Philco",
+    modelo: "XRCTFG-456",
+    preco: 30.0,
+}
+// para acessar uma das caracteriscas -> produto.preco - acessa o preco.
