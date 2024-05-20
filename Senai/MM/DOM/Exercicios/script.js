524d879deeb2a19cc21bd5579cbbe520d2c14a50
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-20 10:58:41 -0300

    feat: exercicio DOM 01

---
+++ Senai/MM/DOM/Exercicios/script.js
@@ -0,0 +1,8 @@
+const adicionarParagrafo = () => {
+    let CriarP = document.createElement("p");
+
+    criarP.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+   document.getElementsByTagName("body").appendChild(CriarP);
+
+}
