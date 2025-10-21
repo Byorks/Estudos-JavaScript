0f74652fd8805c46a924c8f3b895d06290fae8d5
Byork <vanessabyorksp@gmail.com>
2025-10-20 21:10:38 -0300

    :sparkles: feat: pdf with puppeteer

--- Pocs/pdf-puppeter/src/server.js
+++ Pocs/pdf-puppeter/src/server.js
@@ -1,13 +1,20 @@
 // Criando servidor
 import express from "express";
 import ejs from "ejs";
-import path from "path";
+import path, { format } from "path";
 import { fileURLToPath } from "url";
 import pdf from "html-pdf";
 import puppeteer from "puppeteer";
 import fs from "fs";
 const app = express();
-const browser = await puppeteer.launch();
+
+// Deixando o browser aberto até que o servidor seja encerrado
+let browser;
+
+(async ()=> {
+  browser =  await puppeteer.launch();
+})();
+
 // import.meta.url -> Importa o caminho da URL atual
 // fileURLPath -> converte a URL para um caminho de arquivo
 // path.dirname() -> extrai o diretório do arquivo
@@ -34,11 +41,11 @@ const passengers = [
 
 // Essa arrow function será executada ao entrarmos no caminho especificado no primeiro parâmetro
 // request e response é o segredo de ao entrarmos fazemos um pedido e logo recebemos uma resposta
-app.get("/", (request, response) => {
+app.get("/", async (request, response) => {
   // dirname está oculta porque é como se estivessemos executando tudo dentro de uma função e ela está como parâmetro
   // Na real, no ES Modules isso não funciona, então tive que colocar no código
   const filePath = path.join(__dirname, "print.ejs");
-  ejs.renderFile(filePath, { passengers }, (err, html) => {
+  ejs.renderFile(filePath, { passengers }, async (err, html) => {
     if (err) return response.send("Erro na leitura do arquivo");
 
     // Configurando o pdf
@@ -53,13 +60,22 @@ app.get("/", (request, response) => {
       },
     };
 
-    // criar pdf
-    pdf.create(html, options).toFile("report.pdf", (err, data) => {
-      if (err) return response.send("Erro ao gerar o PDF");
+    // Exportando pdf com puppeteer
 
-      // enviar para o navegador
-      return response.send("Arquivo gerado com sucesso!");
-    });
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf({ path: "report.pdf", format: "A4" });
+
+    await browser.close();
+    
+    return response.send("Gerou o pdf, eu espero!");
+    // criar pdf
+    // pdf.create(html, options).toFile("report.pdf", (err, data) => {
+    //   if (err) return response.send("Erro ao gerar o PDF");
+
+    //   // enviar para o navegador
+    //   return response.send("Arquivo gerado com sucesso!");
+    // });
   });
 });
 
@@ -67,4 +83,9 @@ app.get("/", (request, response) => {
 // Se trata de uma função que entra como argumento de outra função
 // Sera executada em um tempo determinado, quero dizer que vai ser determinado ao criar a função
 
+
+// Quando o servidor for encerrado fecha o navegador
+process.on("exit", async () => {
+  if (browser) await browser.close();
+});
 app.listen(3000);
